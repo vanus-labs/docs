86acb60d52164aa1f1fff45f0a6c707b732cade9
wenfeng <sxian.wang@gmail.com>
2023-02-17 18:26:54 +0800

    x

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,6 @@
 import React from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageHeader from '@site/src/components/HomepageHeader';
-import HomepageKeypoints from '@site/src/components/HomepageKeypoints';
-import HomepageIconsBlock from '@site/src/components/HomepageIconsBlock';
-import HomepageBigCard from "../components/HomepageBigCard";
+import {Redirect} from "@docusaurus/router";
 
 export default function Home() {
-    const {siteConfig} = useDocusaurusContext();
-    return (
-        <Layout
-            title={`Vanus `}
-            description="Start your EDA journey with Vanus.">
-            <HomepageHeader />
-            <HomepageKeypoints/>
-            <HomepageIconsBlock/>
-            <HomepageBigCard/>
-        </Layout>
-    );
+    return <Redirect to='/introduction/what-is-vanus' />;
 }
