fcab8d5a09491f269e5e1b03f47979dc56875d93
LancerLucky <62349416+LancerLucky@users.noreply.github.com>
2023-02-21 17:30:42 +0800

    Update docusaurus.config.js
    Co-authored-by: MikeG <mike.giroux22@outlook.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -108,7 +108,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/linkall-labs/docs/edit/main",
+          editUrl: "https://github.com/vanus-labs/docs/edit/main",
         },
         googleAnalytics: {
           trackingID: "G-TJL6X2XKQP",
