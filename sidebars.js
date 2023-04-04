03e83bb042347fd9eddbfdfeb6815852239ea957
Ehigiator Akhile <74439623+ehisakhile@users.noreply.github.com>
2023-04-04 11:49:23 +0100

    Docs s3 sink (#321)
    * docs: Add S3 Sink Docs

--- sidebars.js
+++ sidebars.js
@@ -374,7 +374,8 @@ const sidebars = {
                 'user-guides/connector-guides/sink/feishu-setup',
                 'user-guides/connector-guides/sink/google-sheet-setup',
                 'user-guides/connector-guides/sink/snowflake-setup',
-                'user-guides/connector-guides/sink/display-setup'
+                'user-guides/connector-guides/sink/display-setup',
+                'user-guides/connector-guides/sink/s3-setup'
               ],
             }
           ],
