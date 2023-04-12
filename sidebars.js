200e4f803d7517374fe68dfc41f0c57671dacdff
Ehigiator Akhile <74439623+ehisakhile@users.noreply.github.com>
2023-04-12 07:20:17 +0100

    Docs: Add Elasticsearch Sink Docs (#336)
    Signed-off-by: Ehis Akhile <akhileehis@gmail.com>

--- sidebars.js
+++ sidebars.js
@@ -394,7 +394,8 @@ const sidebars = {
                 'user-guides/connector-guides/sink/display-setup',
                 'user-guides/connector-guides/sink/s3-setup',
                 'user-guides/connector-guides/sink/mysql-setup',
-                'user-guides/connector-guides/sink/slack-setup'
+                'user-guides/connector-guides/sink/slack-setup',
+                'user-guides/connector-guides/sink/elasticsearch-setup'
               ],
             }
           ],
