d789bc7e90b081ecec5e611f54bcad2afbd4d9e5
Yuanchi Wang <wangyuanchidavid@gmail.com>
2023-04-15 00:46:03 +0800

    add image popup

--- static/js/imgbox.js
+++ static/js/imgbox.js
@@ -2,8 +2,8 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
 if (ExecutionEnvironment.canUseDOM) {
     window.onclick = function(){
-        $('img').unbind();
-        $("img").imgbox({
+        $('#__docusaurus img').unbind();
+        $('#__docusaurus img').imgbox({
             zoom:true,
             drag:true,
             scale:1
